b02c4299a1aaee5e7492108fd15dda27a10b901d
SuhaniPatel-915 <148845392+SuhaniPatel-915@users.noreply.github.com>
2025-07-06 20:53:21 +0530

    nodeJs

--- index.js
+++ index.js
@@ -1,2 +1,55 @@
-let img= document.querySelector('img')
-img.setAttribute('src','https://images.unsplash.com/photo-1731243800945-ab7adfde9b8a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')
+// console.log('hello');
+// let fs= require('fs')
+// fs.writeFileSync('index.html','hello')
+// let data= fs.readFileSync('index.html')
+// console.log(data.toString());
+// fs.unlinkSync('index.html')
+// const { log } = require('console')
+//  let express= require('express')
+//  let app=express()
+// app.use((req,res,next)=>{
+//     res.send('nhi jane dunga')
+//     next()
+// })
+// app.use((req,res,next)=>{
+//     // res.send('mai to bilkul jane nhi dungaa')
+//     next()
+// })
+// let express= require('express')
+//  let app=express()
+// app.get('/',(req,res)=>{
+//     res.send('hello mai hu kaun....server hehehe')
+
+// })
+// app.get('/search',(req,res)=>{
+//     let {firstname,lastname}=req.query
+//     res.send(`${firstname} ${lastname}`)
+// })
+// app.get('/:name',(req,res)=>{
+//     let {name}=req.params
+//     res.send(name)
+// })
+ 
+
+// app.get('about',(req,res)=>{
+//    res.send('kyaa bhej duu')
+//  })
+// app.listen(4000,()=>{
+//     console.log('server running on port no. 4000');
+// })
+let express =require('express')
+let mongoose=require('mongoose')
+mongoose.connect('mongodb://127.0.0.1:27017/BGIEM').then(()=>{
+    console.log('chal gyaa....db');
+    
+}).catch((err)=>{
+    console.log(err);
+    
+})
+let app=express()
+app.get('/',(req,res)=>{
+    res.send('hello')
+})
+app.listen(4000,()=>{
+         console.log('server running on port no. 4000');
+ })
